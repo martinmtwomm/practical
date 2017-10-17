d0715f87c44c24974667185156d9eec080ae5181
Jacques Militello <jacques.militello@gmail.com>
2017-10-17 08:22:25 +0200

    add 5 & 6

---
+++ P-0005/src/test/java/io/practical/p0005/P0005Test.java
@@ -0,0 +1,11 @@
+package io.practical.p0005;
+
+import org.junit.Test;
+
+public class P0005Test {
+	
+	@Test
+	public void test() {
+		UnsafeHelper.getUnsafe();
+	}
+}
---
+++ P-0006/src/test/java/io/practical/p0006/P0006Test.java
@@ -0,0 +1,11 @@
+package io.practical.p0006;
+
+import org.junit.Test;
+
+public class P0006Test {
+	
+	@Test
+	public void test() {
+		UnsafeHelper.getUnsafe();
+	}
+}
